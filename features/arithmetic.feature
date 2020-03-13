13564b52e62ba1606f924d65ed2fd698fac5c862
Esperanza Rodriguez <eramos@l-sfmkknjg5j-m.gap.com>
2020-03-12 19:16:48 -0700

    first commit

---
+++ features/arithmetic.feature
@@ -0,0 +1,40 @@
+Feature: Arithmetic
+
+  Scenario Outline: Add two number
+    Given the numbers <number1> and <number2>
+    When they are added together
+    Then should the result be <result>
+    Examples:
+    | number1 | number2 | result |
+    | 1  | 3  | 4  |
+    | 2  | 4  | 6  |
+
+
+Scenario Outline: Substract two number
+    Given the numbers <number1> and <number2>
+    When they are subtracted together
+    Then should the result be <result>
+    Examples:
+    | number1 | number2 | result |
+    | 4  | 2  | 2  |
+    | 6  | 1  | 5  |
+
+
+Scenario Outline: Multiply two number
+    Given the numbers <number1> and <number2>
+    When they are multiplied together
+    Then should the result be <result>
+    Examples:
+    | number1 | number2 | result |
+    | 6  | 2  | 12  |
+    | 2  | 4  | 8  |
+
+
+Scenario Outline: Divide two number
+    Given the numbers <number1> and <number2>
+    When they are divided together
+    Then should the result be <result>
+    Examples:
+    | number1 | number2 | result |
+    | 8  | 2  | 4  |
+    | 6  | 2  | 3  |
---
+++ features/step_definitions/addition_steps.js
@@ -0,0 +1,35 @@
+
+         const assert =  require('assert')
+         const { Before, Given, When, Then } = require('cucumber')
+         const Calculator = require('../../lib/calculator');
+
+         let calculator;
+         
+         Given('the numbers {int} and {int}', function (int1, int2) { 
+              calculator = new Calculator(int1,int2);
+            });
+
+        When('they are added together', function () {
+            // Write code here that turns the phrase above into concrete actions
+            calculator.add();
+            });
+
+        When('they are subtracted together', function () {
+            // Write code here that turns the phrase above into concrete actions
+            calculator.subtract();
+            });
+
+        When('they are multiplied together', function () {
+            // Write code here that turns the phrase above into concrete actions
+            calculator.multiply();
+            });
+
+        When('they are divided together', function () {
+            // Write code here that turns the phrase above into concrete actions
+            calculator.divide();
+            });
+
+
+        Then('should the result be {int}', function (int) {
+            assert.equal(calculator.getResult(),int);
+            });
